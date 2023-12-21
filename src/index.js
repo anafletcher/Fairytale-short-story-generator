01e6731194c941293aa12efd6a73832102e0b464
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-21 20:15:39 +0000

    Improved the user experience and finalised the layout and styling

--- src/index.js
+++ src/index.js
@@ -15,13 +15,13 @@ function generateStory(event) {
   let apiKey = "f3c5613898a1043cbte4a77d8c1bcfo0";
   let context =
     "You are a fairytale expert and are knowledgeable about all the fairytales in the world so please be polite and concise and provide a good fairytale";
-  let prompt = `Please generate a good, entertaining, short fairytale, only 2 paragraphs long, about ${themeInput.value}. Please display the fairytale starting with "Once upon a time" in 2 paragraphs in example HTML format:<p>Once upon a time</p><p>The was a fairytale</p>`;
+  let prompt = `Please generate a good, entertaining, short fairytale, only 12 lines long, about ${themeInput.value}. Please display the fairytale starting with "Once upon a time" in 12 lines`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let storyElement = document.querySelector("#story");
   storyElement.classList.remove("hidden");
-  storyElement.innerHTML = `Generating a "${themeInput.value}" fairytale for you...`;
-
+  storyElement.innerHTML = `<div class="blink"><em>Generating a "${themeInput.value}" fairytale for you...</em></div>`;
+  console.log(themeInput.value);
   axios.get(apiUrl).then(displayStory);
 }
 
