959bc74ac58fd61ef523a6d53d571fa2453533d7
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-21 15:13:27 +0000

    Added basic JS form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateStory(event) {
+  event.preventDefault();
+  let storyElement = document.querySelector("#story");
 
+  new Typewriter("#story", {
+    strings: "Once upon a time",
+    autoStart: true,
+    delay: 10,
+    cursor: null,
+  });
+}
+
+let storyFormElement = document.querySelector("#story-generator-form");
+storyFormElement.addEventListener("submit", generateStory);
