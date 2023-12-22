be9989cd26f2d805dedf3ad93e753fe183b0a3de
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-22 22:18:01 +0000

    modified AI context and prompt

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ function generateStory(event) {
 
   let apiKey = "f3c5613898a1043cbte4a77d8c1bcfo0";
   let context =
-    "You are a fairytale expert and are knowledgeable about all the fairytales in the world so please be polite and concise and provide a good fairytale";
-  let prompt = `Please generate a good, entertaining, short fairytale, only 12 lines long, about ${themeInput.value}. Please display the fairytale starting with "Once upon a time" in 12 lines`;
+    "You are knowledgeable about all the fairytales in the world so please be polite and concise and provide a good fairytale";
+  let prompt = `Please generate a good, entertaining, short fairytale, only 2 paragraphs long, about ${themeInput.value}. Please display the fairytale using the example HTML format: <p>This is a fairytale</p> and starting with "Once upon a time" in 12 lines`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let storyElement = document.querySelector("#story");
